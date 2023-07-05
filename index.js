af40ff936fcd072940ab37504bf8232c3aa75002
caiomelotec <caio.melotec@icloud.com>
2023-07-05 21:35:30 +0200

    clean selectText()

--- index.js
+++ index.js
@@ -9,18 +9,8 @@ const texts = [
 ];
 
 function selectText() {
-  // generate a random number
-  const randomNumber = Math.floor(Math.random() * texts.length);
-
-  console.log(randomNumber);
-  // Create a new <style> element
-  const styleElement = document.createElement("style");
-
-  // Define the CSS rules
-  let cssRules = `.btn.test-completed::before {content: "${texts[randomNumber]}";}`;
-
-  // Set the CSS rules inside the <style> element
-  styleElement.appendChild(document.createTextNode(cssRules));
-  // Append the <style> element to the <head> section of the document
-  document.head.appendChild(styleElement);
+  const styleElement = document.querySelector("style");
+  let randomNumber = Math.floor(Math.random() * texts.length);
+  let cssRules = `#btn-glück::before {content: "${texts[randomNumber]}";}`;
+  styleElement.innerText = cssRules;
 }
